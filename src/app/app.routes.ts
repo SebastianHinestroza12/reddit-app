d2c6da3e1bd50fab9343aefbe4160e0e4c3cc23f
SebastianHinestroza12 <menas7527@gmail.com>
2024-12-09 01:41:58 -0500

    feat: add initial setup for routing, global styles, and reusable components
    - Added Bootstrap CSS to angular.json for global styling.
    - Defined global font-family and scroll behavior in styles.scss.
    - Configured routing module with routes for HomeComponent and RedditDetailComponent.
    - Integrated HeaderComponent and FooterComponent in app.component.ts and app.component.html.
    - Updated app.component.scss to include outlet-container styling.
    - Enhanced app.config.ts to provide HttpClient.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { RedditDetailComponent } from './pages/reddit-detail/reddit-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'subreddit/:id', component: RedditDetailComponent },
+  { path: '**', redirectTo: '' }, // Redirige cualquier ruta desconocida al Home
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
